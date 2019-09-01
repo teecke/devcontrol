9f9c3c75944016559bf7f02f44886fc4dce78461
Pedro Amador <pedroamador.rodriguez@gmail.com>
2019-09-01 15:16:59 +0200

    Fix: Use interactive docker call in tests

--- bin/test.sh
+++ bin/test.sh
@@ -4,7 +4,7 @@ exitCode=0
 for file in devcontrol.sh bin/test.sh; do
     echo -n "Executing shellcheck over ${file}..."
     failed=0
-    docker run -t --workdir /workspace -v "$(pwd)":/workspace -ti koalaman/shellcheck-alpine shellcheck -x "${file}" || failed=1
+    docker run -i --workdir /workspace -v "$(pwd)":/workspace -ti koalaman/shellcheck-alpine shellcheck -x "${file}" || failed=1
     if [ ${failed} -eq 0 ]; then
         echo "[OK]"
     else
